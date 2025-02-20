240c590498b8b94c7be76ecb603cc5834696bce8
Shubham <40598861+vijit101@users.noreply.github.com>
2025-02-20 18:47:22 +0530

    udpated qr code
    qr code appears for all products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,6 +2,7 @@ import path from'path';
 import ProductModel from '../models/product.model.js';
 import { error } from 'console';
 
+
 export default class ProductController{
     getproducts(req,res){
         let prods = ProductModel.get();
@@ -25,12 +26,13 @@ export default class ProductController{
         res.render("new-product",{errorMessage:null});
     }
 
-    addNewProduct(req,res){
-        ProductModel.append(req.body); // req . body gets the prod data in obj form 
-        // makeqr(url)
+    async addNewProduct(req, res) {
+        await ProductModel.append(req.body);  // Wait for QR code generation
         let prod = ProductModel.get();
-        return res.render("products",{prods:prod}); // make sure the key is prods as its used in product ejs better to use an enum and save it as prods 
+        await ProductController.saveFileToModel(prod); // Wait for save
+        return res.render("products", { prods: prod });
     }
+    
 
     // addNewProduct(req,res){
     //     console.log(JSON.stringify(req.body));
@@ -42,6 +44,7 @@ export default class ProductController{
     addProductViaApi(req,res){
         console.log(JSON.stringify(req.body));
         ProductModel.append(req.body); // req . body gets the prod data in obj form 
+        ProductController.saveFileToModel(prod);
         res.send("data updated successfully");
         //res.send("Product information updated successfully");
         
@@ -61,10 +64,27 @@ export default class ProductController{
         }
     }
 
+    getproductbyid(req,res){
+        const id = req.params.id;
+        const prodFound = ProductModel.getByID(id);
+        res.status(200).send(prodFound);
+    }
+
+    updateStockOnPurchase(req,res){
+        const id = req.params.id;
+        const prodFound = ProductModel.getByID(id);
+        prodFound.stockLeft --;
+        let prod = ProductModel.get();
+        saveFileToModel(prod);
+        res.send("stock updated for item :"+prodFound.name);
+
+    }
+
     postUpdateProduct(req,res){
         console.log(req.body);
         ProductModel.update(req.body); // req . body gets the prod data in obj form 
         let prod = ProductModel.get();
+        ProductController.saveFileToModel(prod);
         return res.render("products",{prods:prod});
     }
 
@@ -74,13 +94,21 @@ export default class ProductController{
         if(prodFound){
             ProductModel.delete(id);
             let prod = ProductModel.get();
+            ProductController.saveFileToModel(prod);
             return res.render("products",{prods:prod});
         }
         else{
             res.status(401).send("product not found");
-        }
+        }   
+    }
 
-        
-        
+    static async saveFileToModel(latestprod){
+        //let prod = ProductModel.get();
+        //console.log(latestprod);
+        await ProductModel.saveData(latestprod);
+    }
+
+    LoadData(){
+        ProductModel.readDataSync();
     }
 } 
